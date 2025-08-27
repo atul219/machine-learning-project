a1abaf64f176bcd0fbbcfdb57125f19fd2ef42e8
atul219 <atulyadav219@gmail.com>
2025-08-27 13:57:49 +0100

    docker file updated

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
-FROM python:3.10-slim-buster
+FROM python:3.10-slim
 WORKDIR /app
 COPY . /app
 
 RUN apt update -y && apt install awscli -y
 
 RUN pip install -r requirements.txt
-CMD ["Python3", "app.py"]
+
+# Expose the port
+EXPOSE 5000
+
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -40,16 +40,16 @@ def predict_datapoint():
 
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", debug=True)
+    app.run(host = "0.0.0.0", port=5000)
     
 
-    data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+    # data=CustomData(
+    #         gender=request.form.get('gender'),
+    #         race_ethnicity=request.form.get('ethnicity'),
+    #         parental_level_of_education=request.form.get('parental_level_of_education'),
+    #         lunch=request.form.get('lunch'),
+    #         test_preparation_course=request.form.get('test_preparation_course'),
+    #         reading_score=float(request.form.get('writing_score')),
+    #         writing_score=float(request.form.get('reading_score'))
 
-        )
+    #     )
